9e7fa0e9a39f29ca956797a738fdcb02add31013
NerminEliyeva <narminahmedova1997@gmail.com>
2023-02-12 19:59:15 +0400

    start

---
+++ Customer.DAL/Concrete/EntityFramework/CustomersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.DAL.Concrete.EntityFramework
+{
+    public class CustomersDbContext : DbContext
+    {
+        public CustomersDbContext(DbContextOptions<CustomersDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
---
+++ Customer.Models/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Models.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerPosition { get; set; }
+        public int CustomerCity { get; set; }
+        public double CustomerSalary { get; set; }
+        public int CustomerStatus { get; set; }
+
+    }
+}
